68e5ee5ff06043a5da57ec92fc4b03b81a7755a4
wardenlym <wardenlym@gmail.com>
2019-04-19 11:37:37 +0800

    change document and sample for testing

--- artifacts/poc/install-cni.sh
+++ artifacts/poc/install-cni.sh
@@ -2,7 +2,6 @@
 
 VERSION="v0.1.0"
 
-sudo rm -rf /opt/cni/bin
 sudo mkdir -p /opt/cni/bin
 
 wget https://github.com/containernetworking/plugins/releases/download/v0.7.5/cni-plugins-amd64-v0.7.5.tgz
@@ -12,10 +11,10 @@ wget https://github.com/intel/multus-cni/releases/download/v3.2/multus-cni_3.2_l
 tar zxvf multus-cni_3.2_linux_amd64.tar.gz 
 sudo cp multus-cni_3.2_linux_amd64/multus-cni /opt/cni/bin/
 
-wget https://github.com/wardenlym/static-macvlan-cni/releases/download/$VERSION/static-macvlan-cni-amd64-$VERSION.tgz
+wget https://github.com/cnrancher/static-macvlan-cni/releases/download/$VERSION/static-macvlan-cni-amd64-$VERSION.tgz
 sudo tar zxvf static-macvlan-cni-amd64-$VERSION.tgz -C /opt/cni/bin/
 
-wget https://github.com/wardenlym/static-ipam/releases/download/$VERSION/static-ipam-amd64-$VERSION.tgz
+wget https://github.com/cnrancher/static-ipam/releases/download/$VERSION/static-ipam-amd64-$VERSION.tgz
 sudo tar zxvf static-ipam-amd64-$VERSION.tgz -C /opt/cni/bin/
 
 rm cni-plugins-amd64-v0.7.5.tgz
